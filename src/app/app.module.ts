e7719f6fa6b956d09beec9a7c3ae3454fec367b9
ChisKien <chiskien03214@gmail.com>
2021-10-30 11:42:44 +0700

    import shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {CoreModule} from "./core/core.module";
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {SharedModule} from "./shared/shared.module";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
@@ -12,9 +12,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
